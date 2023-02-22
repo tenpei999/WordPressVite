974acd5aa7883881c24e91e9f0371b20e0c25736
tenpei999 <tenpei999@gmail.com>
2023-02-22 23:25:12 +0900

    [faat]first-commit

---
+++ src/js/front.js
@@ -0,0 +1,137 @@
+//ローディングアニメーション
+window.onload = function () {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
+
+media();
+window.addEventListener('resize', function () { media(); })
+
+
+// メディアクエリ
+function media() {
+  // 横幅を取得
+  let width = $(window).width();
+  if (width > 1200) {
+    // 画面幅が1200pxより上の時
+
+    document.querySelector('.c-text--link.c-modal-window__button-open').parentNode.insertBefore(document.getElementById("modal-text"),
+      document.querySelector('.c-text--link.c-modal-window__button-open').nextElementSibling);
+
+  } else if (width <= 1200) {
+    // 画面幅が1200px以下での時
+
+    //アコーディオンメニュー
+    //クリックで動く
+    document.querySelector(".p-gmenu__button").addEventListener('click', () => {
+      document.querySelector(".p-gmenu__button").classList.toggle('active');
+      document.querySelector(".p-gmenu--table").classList.toggle('c-background--opacity');
+      document.querySelector(".p-gmenu__list.in-table").classList.toggle('active');
+    })
+
+    document.querySelector(".p-modal-window__button-close").parentNode.insertBefore(document.getElementById("modal-text"),
+      document.querySelector(".p-modal-window__button-close").nextElementSibling);
+
+    document.querySelector('.c-text--link.c-modal-window__button-open').addEventListener("click", () => {
+      jQuery('.p-modal-window, .c-overlay, .p-modal-window__message-close').fadeIn();
+      document.querySelector('.p-page-top').style.display = 'none';
+    });
+    document.querySelector('.p-modal-window__message-close').addEventListener("click", () => {
+      jQuery('.p-modal-window, .p-modal-window__message-close, .c-overlay').fadeOut();
+      document.querySelector('.p-page-top').style.display = 'unset';
+    });
+  }
+}
+
+//ページ内リンク
+// jQuery(function () {
+//   // #で始まるアンカーをクリックした場合に処理
+//   jQuery('.p-gmenu__list.in-table li a[href^="#"]').click(function () {
+
+//     //ページ上端からの距離を検知
+//     let winHeight = window.pageYOffset;                             
+//     // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
+//     let adjust = 0;
+//     // スクロールの速度
+//     let speed = 400; // ミリ秒
+//     // アンカーの値取得
+//     let href = jQuery(this).attr("href");
+//     // 移動先を取得
+//     let target = jQuery(href == "#" || href == "" ? 'html' : href);
+//     // 移動先を調整
+//     let position = target.offset().top - adjust;
+//     // スムーススクロール
+//     jQuery('body,html, .l-main').animate({ scrollTop: position }, speed, 'swing');
+//     return false;
+//   });
+// });
+
+//視線に追従する雲
+window.onscroll = function () {
+                          //ページ上端からの距離を検知
+  let elmOffsetY = 0;
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  let position = elmOffsetY + scrollTop / 1.9;
+  let Position2 = elmOffsetY + scrollTop / 1.5;
+
+  document.querySelector('.layer-10').style.marginTop = position + 'px';
+
+  document.querySelector('.layer-11').style.marginTop = Position2 + 'px';
+
+};
+
+
+//前景の山が迫り上がる。
+function init() {
+  window.addEventListener('load', () => {
+    document.querySelector('.layer-2').style.marginTop = '3030px'
+    document.querySelector('.layer-2-2').style.marginTop = '2660px'
+    document.querySelector('.layer-2-3').style.marginTop = '2550px'
+  })
+
+  window.addEventListener('scroll', () => {
+    document.querySelector('.layer-2').style.marginTop = 'unset'
+    document.querySelector('.layer-2-2').style.marginTop = 'unset'
+    document.querySelector('.layer-2-3').style.marginTop = 'unset'
+  })
+
+  'scroll load'.split(' ').forEach((MountainUp) => {
+    window.addEventListener(MountainUp, () => {
+
+      let winHeight = window.pageYOffset;                              //ページ上端からの距離を検知
+      let elmOffsetY = 2250;                                            //'.layer-2'のY座標における絶対位置
+      let winMath = 780 + elmOffsetY;
+      let win2Math = 410 + elmOffsetY;
+      let win3Math = 250 + elmOffsetY;
+      let offsetMove = winMath - winHeight * 0.3333;                             // 'layer-2'のmargin-top を求める条件式
+      let offset2Move = win2Math - winHeight * 0.15;                             // 'layer-2'のmargin-top を求める条件式
+      let offset3Move = win3Math - winHeight * 0.1;                             // 'layer-2'のmargin-top を求める条件式
+
+      // console.log(winHeight);
+      // console.log(winMath);
+      // console.log(elmOffsetY);
+      // console.log(offsetMove);     
+      // console.log("hoge");       
+
+      if (winHeight < 2000) {
+        document.querySelector('.layer-2').classList.add('up');
+        document.querySelector('.layer-2.up').style.marginTop = offsetMove + 'px';
+        document.querySelector('.layer-2-2').classList.add('up');
+        document.querySelector('.layer-2-2.up').style.marginTop = offset2Move + 'px';
+        document.querySelector('.layer-2-3').classList.add('up');
+        document.querySelector('.layer-2-3.up').style.marginTop = offset3Move + 'px';
+
+        //アニメーションをする前の状態を表すクラス名'.upper'を付与し、合わせて'layer-2'を下方に待機させる。
+      } else {
+        document.querySelector('.layer-2').classList.remove('up');
+        document.querySelector('.layer-2').style.marginTop = '2360px';
+        document.querySelector('.layer-2-2').classList.remove('up');
+        document.querySelector('.layer-2-2').style.marginTop = '2360px';
+        document.querySelector('.layer-2-3').classList.remove('up');
+        document.querySelector('.layer-2-3').style.marginTop = '2300px';
+      }
+    })
+  })
+};
+
+init();
---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+// 画面をスクロールをしたら動かしたい場合の記述
+// .p-page-topをクリックした際の設定
+
+
+jQuery(function () {
+  jQuery(".p-page-top").on("click", function () {
+    const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    //スクロール値を取得
+    if (scroll > 0) {
+      document.querySelector('.p-page-top').classList.add('floatAnime');
+      //クリックしたらfloatAnimeというクラス名が付与
+      jQuery('body,html,.l-main').animate({ scrollTop: 0 }, 3000, 'swing', function () {
+        //スクロールの速さ。数字が大きくなるほど遅くなる
+        document.querySelector('.p-page-top').classList.remove('floatAnime');
+      });
+    }
+    return false;//リンク自体の無効化
+  });
+
+});
+
+
+'scroll load'.split(' ').forEach((inokichiDown) => {
+  window.addEventListener(inokichiDown, () => {
+
+    let winHeight = window.pageYOffset;
+    if (winHeight >= 100) {//上から100pxスクロールしたら
+      document.querySelector('.p-page-top').classList.remove('DownMove');//#page-topについているDownMoveというクラス名を除く
+      document.querySelector('.p-page-top').classList.add('UpMove');//#page-topについているUpMoveというクラス名を付与
+    } else {
+      //すでに#page-topにUpMoveというクラス名がついていたら
+      document.querySelector('.p-page-top').classList.add('UpMove');//UpMoveというクラス名を除き
+      document.querySelector('.p-page-top').classList.add('DownMove');//DownMoveというクラス名を#page-topに付与
+    }
+  })
+})
+
+//ローディングアニメーション
+window.onload = function () {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
+
+//ブール値 true or foals //データ型 ○ ストリング(文字列)　イント(数字) //リスト型○ //タイプスプリクト //視認性が悪い ○ //条件式 //イベントトリガー
+//`hoge = ${scroll < trigger && scroll < goal
+//リスト型
+
+  //数値
+  //移動前の位置 layer-2(margin-top: 2692px;) window(.scrollTop(): 1270px;) -1422px
+  //移動後の位置 layer-2(margin-top: 2360px;) window(.scrollTop(): 1602px;) -758px
+  //                                 -332px                        +332px   -664
